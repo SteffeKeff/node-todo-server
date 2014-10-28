98a6538f53443ff3ffada01994ef1f6fa0e539d3
Stefan De Geer <sdgkeff@gmail.com>
2014-10-28 08:44:45 +0100

    till skolan

--- public/js/script.js
+++ public/js/script.js
@@ -9,11 +9,8 @@ $(function () {
             removeButton = $('<span>remove</span>');
         removeButton.addClass('remove');
         removeButton.click(function () {
-            // 4) Delete the item from the server itemObj.id
-            // delete http://127.0.0.1:8001/items/itemObj.id
             $.ajax({
-                //url: 'http://127.0.0.1:8001/items/' + items.indexOf(removeButton.siblings().text()),
-                url: 'http://83.251.232.254:8001/items/' + items.indexOf(removeButton.siblings().text()),
+                url: '/items/' + items.indexOf(removeButton.siblings().text()),
                 type: 'delete',
                 success: function () {
                     //console.log('Deleted');
@@ -21,10 +18,6 @@ $(function () {
                 error: function (errorThrown) {
                     console.log('Error: ' + errorThrown);
                 },
-                complete: function () {
-                    console.log('The delete request is complete');
-                }
-
             });
             listItem.remove();
             // 5) Instead of updateItems rerun loadItems
@@ -42,18 +35,11 @@ $(function () {
         $('ul').empty();
         items.length = 0;
         // 1) Change this function to get data from the server with ajax instead of localstorage.
-        // get http://127.0.0.1:8001/items
         $.ajax({
-            //url: 'http://127.0.0.1:8001/items',
-            url: 'http://83.251.232.254:8001/items',
+            url: '/items',
             type: 'get',
             dataType: 'JSON',
             success: function (json) {
-                //onödigt med 2 for loopar?
-                // for (var key in json) {
-                //     items.push(json[key]);
-                // }
-                //for (var i = 0; i < items.length; i = i + 1) {
                 for (var i = 0; i < json.length; i++) {
                     items.push(json[i]);
                     addItem(json[i]);
@@ -64,10 +50,6 @@ $(function () {
                 console.log('Status: ' + status);
                 console.dir(xhr);
             },
-            complete: function () {
-                console.log('Loading items is complete');
-            }
-
         });
     };
 
@@ -79,27 +61,14 @@ $(function () {
         };
         if (itemText !== '') {
             // 2) add the item to the server with ajax
-            // post http://127.0.0.1:8001/items
             $.ajax({
-                //url: 'http://127.0.0.1:8001/items',
-                url: 'http://83.251.232.254:8001/items',
+                url: '/items',
                 type: 'post',
                 dataType: 'JSON',
                 contentType: 'application/json; charset=utf-8',
                 data: JSON.stringify(jsonObject),
-                success: function (data) {
-                    //console.log('success');
-                },
-                error: function (xhr, status, errorThrown) {
-                    //console.log('some weird error');
-                    //console.log('Error: ' + errorThrown);
-                    //console.log('Status: ' + status);
-                    //console.dir(xhr);
-                },
-                complete: function () {
-                    console.log('New item is added to the list');
-                }
-
+                success: function (data) {},
+                error: function (xhr, status, errorThrown) {},
             });
             addItem({
                 name: itemText
